78c75242cb6d0c386f64673d2463111eb4087532
HOANGLAM2952002 <tommy2952002@gmail.com>
2020-04-16 08:09:10 +0700

    remove folder frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header, Footer } from './components/layout/index';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
