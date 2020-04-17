768a5484fd4965eef8300f14382d132712077e17
unknown <nguyenhoangminh191202@gmail.com>
2020-04-18 00:28:55 +0700

    add map to ncov page

--- src/components/content/covid19_data.js
+++ src/components/content/covid19_data.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import CovidMap from './covid19_data/index';
+
+import axios from 'axios';
 
 const Covid19_data = () => {
+  const [loading, setLoading] = useState(false)
+  const [covidData, setCovidData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async function () {
+      setLoading(true)
+      const result = await axios.get('https://corona.lmao.ninja/v2/countries')
+      setCovidData(result.data)
+      setLoading(false)
+    }
+    fetchData()
+  }, [])
+  
   return (
     <div>
-      Covid 19 Data
+      { 
+        loading
+        ? <div>...Loading</div>
+        : <CovidMap data={covidData}/>
+      }
     </div>
   )
 }
