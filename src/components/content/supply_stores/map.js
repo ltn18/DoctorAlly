73d5ee7ae290dfb7defc48d619bb25e957e33404
HOANGLAM2952002 <tommy2952002@gmail.com>
2020-04-17 22:35:54 +0700

    update privacy_policy & terms_of_use

--- src/components/content/supply_stores/map.js
+++ src/components/content/supply_stores/map.js
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({text}) => <div>{text}</div>;
-
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const KEY = 'AIzaSyAWvAWfrCh1e1anFy-ucRdryT0uLh38QL0';
+const ALT_KEY = "AIzaSyCw1Cu5QmZqsFLWq-D7m12E3Qqjjj13xWY";
 const Maps = () => {
-    const [center, setCenter] = useState({lat: 11.0168, lng: 76.9558 });
-    const [zoom, setZoom] = useState(11);
-    return (
-        <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAWvAWfrCh1e1anFy-ucRdryT0uLh38QL0'}}
-          defaultCenter={center}
-          defaultZoom={zoom}
-        >
-          <AnyReactComponent
-            lat={11.0168}
-            lng={76.9558}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-    );
+  const [center, setCenter] = useState({ lat: 11.0168, lng: 76.9558 });
+  const [zoom, setZoom] = useState(11);
+  return (
+    <div style={{ height: '50vh', width: '100%', marginTop: 20 }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: KEY}}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        <AnyReactComponent
+          lat={11.0168}
+          lng={76.9558}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  );
 }
 
 export default Maps;
