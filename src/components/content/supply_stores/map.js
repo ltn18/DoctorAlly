d430550927d80c9b45703960b9ec30a10b9d9996
unknown <nguyenhoangminh191202@gmail.com>
2020-04-20 19:21:44 +0700

    update maps

--- src/components/content/supply_stores/map.js
+++ src/components/content/supply_stores/map.js
@@ -1,78 +1,169 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-const [__lng, __lat, __zoom] = [105.8380, 21.0269, 14.52]; // Hanoi
-const TOKEN = "pk.eyJ1IjoiaG9hbmdtaW5obmciLCJhIjoiY2s5M25xYTMwMDRhZDNpcDNhOHN1cDRnciJ9.NvYOhaROmMb04qeJyIbG-A";
-mapboxgl.accessToken = TOKEN;
+import axios from 'axios';
 
-class SupplyStoreMap extends Component {
+mapboxgl.accessToken = 'pk.eyJ1IjoiaG9hbmdtaW5obmciLCJhIjoiY2s5M25xYTMwMDRhZDNpcDNhOHN1cDRnciJ9.NvYOhaROmMb04qeJyIbG-A';
+const [lng, lat, zoom] = [105.8380, 21.0269, 14.52]; // Hanoi
+const [clng, clat, czoom] = [5, 34, 2]; // World
+
+class StoreMap extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      lng: __lng,
-      lat: __lat,
-      zoom: __zoom
-    };
-    this.data = props.data;
-  }
-
-  convertData() {
-    const GeoJsonList = [];
-    if (this.data) {
-      var GeoJson = {
-        'type': 'geojson',
-        'data': {
-          'type': 'FeatureCollection',
-          'features': [],
-        }
-      }
-
-      this.data.map(item => {
-        GeoJson.data.features.push({
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Point',
-            'coordinates': [item.lng, item.lat]
-          },
-          'properties': {
-            'type': item.type,
-            'phone': item.phone,
-            'open': item.time.open,
-            'closed': item.time.closed,
-          }
-        })
-      })
-    }
   }
 
   componentDidMount() {
     const map = new mapboxgl.Map({
-      container: this.mapContainer,
+      container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
+      center: [lng, lat],
+      zoom: zoom,
     });
 
-    map.on('move', () => {
-      this.setState({
-        lng: map.getCenter().lng.toFixed(4),
-        lat: map.getCenter().lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
+    var marker = new mapboxgl.Marker({ 'color': '#008000' })
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      marker: false,
+      placeholder: 'Search for stores near you',
+      proximity: {
+        longitude: lng,
+        lattitude: lat,
+      }
+    })
+    map.addControl(geocoder, 'top-left')
+
+    map.on('load', function () {
+      map.addSource('selected-point', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: []
+        }
       });
-    });
+
+      geocoder.on('result', async function (e) {
+        map.getSource('selected-point').setData(e.result.geometry);
+        var point = e.result.center;
+        var tileset = 'mapbox.mapbox-streets-v8'
+        var radius = 2000; //meters
+        var limit = 50; // The maximum amount of results to return
+        var layers = 'poi_label'
+        var query = 'https://api.mapbox.com/v4/' + tileset + '/tilequery/' + point[0] + ',' + point[1] + '.json?radius=' + radius + '&limit=' + limit + '&layers=' + layers + '&access_token=' + mapboxgl.accessToken;
+        marker.setLngLat(point).addTo(map);
+
+        // fetch data and make geojson data
+        const result = await axios.get(query)
+        result.data.features = result.data.features.filter((item) => {
+          if (item.properties.class === 'store_like' || item.properties.class === 'food_and_drink' || item.properties.class === 'food_and_drink_stores') {
+            return item
+          }
+        })
+        map.getSource('tilequery').setData(result.data);
+
+      });
+
+      map.addSource('tilequery', {
+        type: "geojson",
+        data: {
+          "type": "FeatureCollection",
+          "features": []
+        }
+      });
+
+      map.addLayer({
+        id: "tilequery-points",
+        type: "circle",
+        source: "tilequery",
+        paint: {
+          "circle-stroke-color": "white",
+          "circle-stroke-width": {
+            stops: [
+              [0, 0.1],
+              [18, 3]
+            ],
+            base: 5
+          },
+          "circle-radius": {
+            stops: [
+              [12, 6],
+              [22, 180]
+            ],
+            base: 6
+          },
+          "circle-color": [
+            'match',
+            ['get', 'type'],
+            'Convenience Store', '#FF8C00',
+            'Convenience Store With Gas', '#FF8C00',
+            'Convenience', '#FF8C00',
+            'Pharmacy', '#FF8C00',
+            'Specialty Food Store', '#9ACD32',
+            'Fast Food', '#9ACD32',
+            'Cafe', '#9ACD32',
+            'Bakery', '#9ACD32',
+            'Small Grocery Store', '#008000',
+            'Supercenter', '#008000',
+            'Superette', '#008000',
+            'Supermarket', '#008000',
+            'Warehouse Club Store', '#008000',
+            'Restaurant', '#00FFFF',
+            '#FF0000' // any other store type
+          ]
+        }
+      });
+
+      var popup = new mapboxgl.Popup;
+
+      map.on('mouseenter', 'tilequery-points', function (e) {
+        map.getCanvas().style.cursor = 'pointer';
+        console.log(e)
+        if (e.features[0].properties.name) {
+          var title = '<h3>' + e.features[0].properties.name + '</h3>';
+        } else {
+          var title = '<h3>No Name</h3>';
+        }
+        var storeType = '<h4>' + e.features[0].properties.type + '</h4>';
+        var obj = JSON.parse(e.features[0].properties.tilequery);
+        var distance = '<p>' + (obj.distance).toFixed(0) + 'm from location' + '</p>';
+
+        // var lon = e.features[0].properties.longitude;
+        // var lat = e.features[0].properties.latitude;
+        var coordinates = new mapboxgl.LngLat(e.features[0].geometry.coordinates[0], e.features[0].geometry.coordinates[1]);
+        var content = title + storeType + distance;
+
+        popup.setLngLat(coordinates)
+          .setHTML(content)
+          .addTo(map);
+      })
+
+      map.on('mouseleave', 'tilequery-points', function () {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+      });
+    })
   }
 
   render() {
     return (
-      <div>
-        <div className='sidebarStyle' style={{ margin: '10px' }}>
-          <div><strong>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom} | Hanoi Map</strong></div>
+      <>
+        <div style={{display: 'flex'}}>
+          <div id="geocoder1" class="geocoder"></div>
+          <div id="geocoder2" class="geocoder"></div>
         </div>
-        <div ref={el => this.mapContainer = el} className='mapContainer' style={{ height: '75vh' }} />
-      </div>
+
+        <div id='map' style={{ height: '75vh', width: '100%' }} />
+      </>
     )
   }
 }
 
-export default SupplyStoreMap;
+export default StoreMap;
+
+
+
+
--- src/components/content/supply_stores/supply_stores.js
+++ src/components/content/supply_stores/supply_stores.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import Map from './map';
+import React, { useState } from 'react';
+import StoreMap from './map';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  h2: {
+    marginTop: 50,
+    marginBottom: 40,
+    textAlign: 'center',
+  },
+}));
 
 const Supply_stores = () => {
+  const classes = useStyles()
   return (
-    <Map />
+    <>
+      <div>
+        <h2 className={classes.h2}>Hanoi Supply Map</h2>
+        <p style={{textAlign: 'center'}}>In order to support medical workers, especially those in need of regular supplies such as food and household appliances, we have created an easy-approaching map for supply stores. The only thing you have to do is to enter your location and the nearest shops will appear for you to choose!!</p>
+        <StoreMap />
+      </div>
+    </>
+
   );
 }
 
