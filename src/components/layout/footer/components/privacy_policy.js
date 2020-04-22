d67a7855d5b1a515c98a36e2119a6e0aa1575e96
unknown <nguyenhoangminh191202@gmail.com>
2020-04-22 23:26:13 +0700

    fix terms + policy

--- src/components/layout/footer/components/privacy_policy.js
+++ src/components/layout/footer/components/privacy_policy.js
@@ -1,73 +1,79 @@
 import React, { useContext } from 'react';
 import localeContext, { getLongLineText } from '../../../context/localeCtx';
+import "./style.css";
 
+const WEBSITE_URL = "http://localhost:3000";
 const Privacy_policy = () => {
   const locale = useContext(localeContext);
   return (
     <div>
       <>
-        <h1>{getLongLineText("privacy_policy", "h1", "title", locale.lang)}</h1>
-        <p style={{ fontStyle: 'italic' }}>{getLongLineText("privacy_policy", "h1", "p1", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h1", "p2", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h1", "p3", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h1", "p4", locale.lang)}</p>
+        <h1>{getLongLineText(locale.lang, "privacy_policy", "h1", "title")}</h1>
+        <p style={{ fontStyle: 'italic' }}>{getLongLineText(locale.lang, "privacy_policy", "h1", "p1")}</p>
+        <p>
+          {getLongLineText(locale.lang, "privacy_policy", "h1", "p2_main1")}
+          <a href={WEBSITE_URL}>{WEBSITE_URL}</a>
+          {getLongLineText(locale.lang, "privacy_policy", "h1", "p2_main2")}
+        </p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h1", "p3")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h1", "p4")}</p>
 
-        <h2>{getLongLineText("privacy_policy", "h2_consent", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_consent", "p1", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_consent", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_consent", "p1")}</p>
 
-        <h2>{getLongLineText("privacy_policy", "h2_infoColl", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_infoColl", "p1", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_infoColl", "p2", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_infoColl", "p3", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_infoColl", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_infoColl", "p1")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_infoColl", "p2")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_infoColl", "p3")}</p>
 
-        <h2>{getLongLineText("privacy_policy", "h2_infoUse", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_infoUse", "p1", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_infoUse", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_infoUse", "p1")}</p>
         <ul>
-          <li>{getLongLineText("privacy_policy", "h2_infoUse", "li_1", locale.lang)}</li>
-          <li>{getLongLineText("privacy_policy", "h2_infoUse", "li_2", locale.lang)}</li>
-          <li>{getLongLineText("privacy_policy", "h2_infoUse", "li_3", locale.lang)}</li>
-          <li>{getLongLineText("privacy_policy", "h2_infoUse", "li_4", locale.lang)}</li>
-          <li>{getLongLineText("privacy_policy", "h2_infoUse", "li_5", locale.lang)}</li>
-          <li>{getLongLineText("privacy_policy", "h2_infoUse", "li_6", locale.lang)}</li>
-          <li>{getLongLineText("privacy_policy", "h2_infoUse", "li_7", locale.lang)}</li>
+          <li>{getLongLineText(locale.lang, "privacy_policy", "h2_infoUse", "li_1")}</li>
+          <li>{getLongLineText(locale.lang, "privacy_policy", "h2_infoUse", "li_2")}</li>
+          <li>{getLongLineText(locale.lang, "privacy_policy", "h2_infoUse", "li_3")}</li>
+          <li>{getLongLineText(locale.lang, "privacy_policy", "h2_infoUse", "li_4")}</li>
+          <li>{getLongLineText(locale.lang, "privacy_policy", "h2_infoUse", "li_5")}</li>
+          <li>{getLongLineText(locale.lang, "privacy_policy", "h2_infoUse", "li_6")}</li>
+          <li>{getLongLineText(locale.lang, "privacy_policy", "h2_infoUse", "li_7")}</li>
         </ul>
 
-        <h2>{getLongLineText("privacy_policy", "h2_log", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_log", "p1", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_log", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_log", "p1")}</p>
 
-        <h2>{getLongLineText("privacy_policy", "h2_advertising", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_advertising", "p1", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_advertising", "p2", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_advertising", "p3", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_advertising", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_advertising", "p1")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_advertising", "p2")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_advertising", "p3")}</p>
 
-        <h2>{getLongLineText("privacy_policy", "h2_third_party", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_third_party", "p1", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_third_party", "p2", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_third_party", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_third_party", "p1")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_third_party", "p2")}</p>
 
-        <h2>{getLongLineText("privacy_policy", "h2_third_party", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_third_party", "p1", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_third_party", "p2", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_third_party", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_third_party", "p1")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_third_party", "p2")}</p>
 
-        <h2>{getLongLineText("privacy_policy", "h2_ccpa", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_ccpa", "p1", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_ccpa", "p2", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_ccpa", "p3", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_ccpa", "p4", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_ccpa", "p5", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_ccpa", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_ccpa", "p1")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_ccpa", "p2")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_ccpa", "p3")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_ccpa", "p4")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_ccpa", "p5")}</p>
 
-        <h2>{getLongLineText("privacy_policy", "h2_gdpr", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_gdpr", "p1", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_gdpr", "p2", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_gdpr", "p3", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_gdpr", "p4", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_gdpr", "p5", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_gdpr", "p6", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_gdpr", "p7", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_gdpr", "p8", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_gdpr", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_gdpr", "p1")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_gdpr", "p2")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_gdpr", "p3")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_gdpr", "p4")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_gdpr", "p5")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_gdpr", "p6")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_gdpr", "p7")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_gdpr", "p8")}</p>
 
-        <h2>{getLongLineText("privacy_policy", "h2_childrenInfo", "title", locale.lang)}</h2>
-        <p>{getLongLineText("privacy_policy", "h2_childrenInfo", "p1", locale.lang)}</p>
-        <p>{getLongLineText("privacy_policy", "h2_childrenInfo", "p2", locale.lang)}</p>
+        <h2>{getLongLineText(locale.lang, "privacy_policy", "h2_childrenInfo", "title")}</h2>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_childrenInfo", "p1")}</p>
+        <p>{getLongLineText(locale.lang, "privacy_policy", "h2_childrenInfo", "p2")}</p>
       </>
     </div>
   )
--- src/components/layout/footer/components/terms_of_use.js
+++ src/components/layout/footer/components/terms_of_use.js
@@ -2,82 +2,163 @@ import React, { useContext } from 'react';
 import localeContext, { getLongLineText } from '../../../context/localeCtx';
 
 const WEBSITE_EMAIL = "doctorally.project@gmail.com";
+const WEBSITE_EMAIL_URL = "mailto:doctorally.project@gmail.com"
 const WEBSITE_ADDRESS = "No 1 Hoang Minh Giam St";
-
+const WEBSITE_URL = '"http://localhost:3000"';
 
 const Terms_of_use = () => {
   const locale = useContext(localeContext);
   return (
     <>
-      <h1>{getLongLineText("terms_of_use", "h1", "title", locale.lang)}</h1>
-      <p style={{ fontStyle: 'italic' }}>{getLongLineText("terms_of_use", "h1", "p1", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h1", "p2", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h1", "p3", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h1", "p4", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h1", "p5", locale.lang)}</p>
+      <h1>{getLongLineText(locale.lang, "terms_of_use", "h1", "title")}</h1>
+      <p style={{ fontStyle: 'italic' }}>{getLongLineText(locale.lang, "terms_of_use", "h1", "p1")}</p>
+      <p>
+        {getLongLineText(locale.lang, "terms_of_use", "h1", "p2_main1")}
+        <a href={WEBSITE_URL}>{WEBSITE_URL}</a>
+        {getLongLineText(locale.lang, "terms_of_use", "h1", "p2_main2")}
 
-      <h2>{getLongLineText("terms_of_use", "h2_access", "title", locale.lang)}</h2>
-      <p>{getLongLineText("terms_of_use", "h2_access", "p1", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_access", "p2", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_access", "p3", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_access", "p4", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_access", "p5", locale.lang)}</p>
+      </p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h1", "p3")}</p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h1", "p4")}</p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h1", "p5")}</p>
 
-      <h2>{getLongLineText("terms_of_use", "h2_ads", "title", locale.lang)}</h2>
-      <p>{getLongLineText("terms_of_use", "h2_ads", "p1", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_ads", "p2", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_ads", "p3", locale.lang)}</p>
+      <h2>{getLongLineText(locale.lang, "terms_of_use", "h2_access", "title")}</h2>
+      <p>
+      <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_access", "p1_strong")}</strong>
+      {getLongLineText(locale.lang, "terms_of_use", "h2_access", "p1_main")}
+      </p>
+      <p>
+      <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_access", "p2_strong")}</strong>
+      {getLongLineText(locale.lang, "terms_of_use", "h2_access", "p2_main")}
+      </p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_access", "p3")}</p>
+      <p>
+      <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_access", "p4_strong")}</strong>
+      {getLongLineText(locale.lang, "terms_of_use", "h2_access", "p4_main")}
+      </p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_access", "p5")}</p>
 
-      <h2>{getLongLineText("terms_of_use", "h2_disclaimer", "title", locale.lang)}</h2>
-      <p>{getLongLineText("terms_of_use", "h2_disclaimer", "p1", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_disclaimer", "p2", locale.lang)}</p>
+      <h2>{getLongLineText(locale.lang, "terms_of_use", "h2_ads", "title")}</h2>
+      <p>
+      <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_ads", "p1_strong")}</strong>
+      {getLongLineText(locale.lang, "terms_of_use", "h2_ads", "p1_main")}
+      </p>
+      <p>
+      <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_ads", "p2_strong")}</strong>
+      {getLongLineText(locale.lang, "terms_of_use", "h2_ads", "p2_main")}
+      </p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_ads", "p3")}</p>
 
-      <h2>{getLongLineText("terms_of_use", "h2_limitation", "title", locale.lang)}</h2>
-      <p>{getLongLineText("terms_of_use", "h2_limitation", "p1", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_limitation", "p2", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_limitation", "p3", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_limitation", "p4", locale.lang)}</p>
+      <h2>{getLongLineText(locale.lang, "terms_of_use", "h2_disclaimer", "title")}</h2>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_disclaimer", "p1")}</p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_disclaimer", "p2")}</p>
 
-      <h2>{getLongLineText("terms_of_use", "h2_copyright", "title", locale.lang)}</h2>
-      <p>{getLongLineText("terms_of_use", "h2_copyright", "p1", locale.lang)}</p>
+      <h2>{getLongLineText(locale.lang, "terms_of_use", "h2_limitation", "title")}</h2>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_limitation", "p1")}</p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_limitation", "p2")}</p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_limitation", "p3")}</p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_limitation", "p4_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_limitation", "p4_main")}
+      </p>
+
+      <h2>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "title")}</h2>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "p1")}</p>
       <ul>
-        <li>{getLongLineText("terms_of_use", "h2_copyright", "li_1", locale.lang)}</li>
-        <li>{getLongLineText("terms_of_use", "h2_copyright", "li_2", locale.lang)}</li>
-        <li>{getLongLineText("terms_of_use", "h2_copyright", "li_3", locale.lang)}</li>
-        <li>{getLongLineText("terms_of_use", "h2_copyright", "li_4", locale.lang)}</li>
-        <li>{getLongLineText("terms_of_use", "h2_copyright", "li_5", locale.lang)}</li>
-        <li>{getLongLineText("terms_of_use", "h2_copyright", "li_6", locale.lang)}</li>
-        <li>{getLongLineText("terms_of_use", "h2_copyright", "li_7", locale.lang)}</li>
+        <li>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "li_1")}</li>
+        <li>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "li_2")}</li>
+        <li>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "li_3")}</li>
+        <li>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "li_4")}</li>
+        <li>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "li_5")}</li>
+        <li>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "li_6")}</li>
+        <li>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "li_7")}</li>
       </ul>
-      <p>{getLongLineText("terms_of_use", "h2_copyright", "p2", locale.lang)}</p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_copyright", "p2")}</p>
 
-      <h2>{getLongLineText("terms_of_use", "h2_general", "title", locale.lang)}</h2>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p1", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p2", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p3", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p4", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p5", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p6", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p7", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p8", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p9", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p10", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p11", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p12", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p13", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p14", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p15", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p16", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p17", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p18", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p19", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p20", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p21", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_general", "p22", locale.lang)}</p>
+      <h2>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "title")}</h2>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p1")}</p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p2_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p2_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p3_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p3_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p4_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p4_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p5_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p5_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p6_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p6_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p7_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p7_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p8_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p8_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p9_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p9_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p10_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p10_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p11_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p11_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p12_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p12_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p13_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p13_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p14_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p14_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p15_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p15_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p16_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p16_main")}
+      </p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p17")}</p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p18")}</p>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p19")}</p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p20_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p20_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p21_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p21_main")}
+      </p>
+      <p>
+        <strong>{getLongLineText(locale.lang, "terms_of_use", "h2_general", "p22_strong")}</strong>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_general", "p22_main")}
+      </p>
 
-      <h2>{getLongLineText("terms_of_use", "h2_contact", "title", locale.lang)}</h2>
-      <p>{getLongLineText("terms_of_use", "h2_contact", "p1", locale.lang)}</p>
-      <p>{getLongLineText("terms_of_use", "h2_contact", "p2", locale.lang)}</p>
+      <h2>{getLongLineText(locale.lang, "terms_of_use", "h2_contact", "title")}</h2>
+      <p>{getLongLineText(locale.lang, "terms_of_use", "h2_contact", "p1")}</p>
+      <p>
+        {getLongLineText(locale.lang, "terms_of_use", "h2_contact", "p2")}
+        <a href={WEBSITE_EMAIL_URL}>{WEBSITE_EMAIL}</a>
+      </p>
     </>
   )
 }
